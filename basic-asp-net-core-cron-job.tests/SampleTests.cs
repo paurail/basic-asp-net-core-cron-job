7d2644f4fba5df240821a4b3505f07236a43770d
Paulius Raila <paulius.raila@knab.nl>
2021-01-17 12:37:45 +0200

    Initial commit with Basic cron service

---
+++ basic-asp-net-core-cron-job.tests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace basic_asp_net_core_cron_job.tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ basic-asp-net-core-cron-job/BasicCronHostedService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NCrontab;
+
+namespace basic_asp_net_core_cron_job
+{
+    public class BasicCronHostedService: IHostedService
+    {
+        private readonly CrontabSchedule _crontabSchedule;
+        private DateTime _nextRun;
+        private readonly Func<Task> _action;
+
+        public BasicCronHostedService(Func<Task> action, string schedule)
+        {
+            _action = action;
+            _crontabSchedule = CrontabSchedule.Parse(schedule, new CrontabSchedule.ParseOptions{IncludingSeconds = true});
+            _nextRun = _crontabSchedule.GetNextOccurrence(DateTime.Now);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Run(async () =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(UntilNextExecution(), cancellationToken);
+
+                    await _action();
+
+                    _nextRun = _crontabSchedule.GetNextOccurrence(DateTime.Now);
+                }
+            }, cancellationToken);
+
+            return Task.CompletedTask;
+        }
+
+        private int UntilNextExecution() => Math.Max(0, (int)_nextRun.Subtract(DateTime.Now).TotalMilliseconds);
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
